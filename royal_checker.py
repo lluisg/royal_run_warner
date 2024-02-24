b6693325cd1b722fe4609a4ccc3729c3e0c36310
Lluis Guardia Fernandez <lluis.guardia.f@gmail.com>
2024-02-24 19:45:44 +0100

    updated with notifications
    now sens notification on pushbullet when running

--- royal_checker.py
+++ royal_checker.py
@@ -1,50 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
-import yagmail
 from dotenv import load_dotenv
 import os
+import time
+from pushbullet import Pushbullet
 
 # Function to scrape the website and check for certain conditions
 def check_website(url):
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, 'html.parser')
-  # print(soup.encode('utf-8'))
-  return True
-  if "Der findes ingen billetter til salg" in soup.get_text():
-    # "Tickets are sold out"
-    return False
-  else:
-    # "Tickets are available"
-    return True
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    if "Der findes ingen billetter til salg" in soup.get_text():
+        # "Tickets are sold out"
+        return False
+    else:
+        # "Tickets are available"
+        return True
 
-# Function to send email with the results
-def send_email(subject, body):
-  load_dotenv()
+def send_notification(title, body):
+    API_KEY = os.getenv("API_KEY")
+    print(API_KEY)
+    pb = Pushbullet(API_KEY)
+    push = pb.push_note(title, body)
 
-  MAIL_SENDER = "lluis.guardia.f@gmail.com"
-  PASS_SENDER = os.getenv("MAIL_PASSWORD")
-
-  receiver = "lluis.guardia.f@gmail.com"
-
-  yag = yagmail.SMTP(MAIL_SENDER, PASS_SENDER)
-  
-  yag.send(
-      to=receiver,
-      subject=subject,
-      contents=body, 
-  )
-
-# Main function to check website and send email every 10 minutes
+# Main function to check website and send notification every 30 seconds
 def main():
-  url5km = 'https://www.sportstiming.dk/event/13416/resale?distance=72951'
-  url10km = 'https://www.sportstiming.dk/event/13416/resale?distance=72952'
+    TIME = 10
+    URL = 'https://www.sportstiming.dk/event/13416/resale'
 
-  result5km = check_website(url5km)
-  result10km = check_website(url10km)
-  print('5km:', result5km, ', 10km:', result10km)
+    try:
+        while True:
+            result = check_website(URL)
 
-  if result5km or result10km:
-    send_email("Ticket For Sale", 'Tickets for 5km:'+str(result5km)+'\nTickets for 10km:'+str(result10km))
+            if result:
+                print('--- result on', time.strftime("%H:%M:%S"), ':', result, '---')
+                event_title = "On sale!"
+                event_body = "Royal run on sale!"
+                send_notification(event_title, event_body)
+            else:
+                print('result on', time.strftime("%H:%M:%S"), ':', result)
+
+            time.sleep(TIME)  # Sleep for 30 seconds before checking again
+
+    except KeyboardInterrupt:
+        print("Script stopped by user.")
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
